bfa501acad4ffddc8a08025bdcadb1482ffefe90
TylerLN <tylerlngu@csu.fullerton.edu>
2024-12-15 17:28:41 -0800

    Format

--- pipes.c
+++ pipes.c
@@ -1,3 +1,4 @@
+#include "redir.h"
 #include <fcntl.h>
 #include <limits.h>
 #include <stdio.h>
@@ -5,85 +6,83 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include "redir.h"
 
 #ifndef PATH_MAX
 #define PATH_MAX 1000
 #endif
 
 int main(int argc, char *argv[]) {
-  if (argc < 4) {
-    fprintf(stderr, "Usage: %s <input_file> <command> <output_file>\n",
-            argv[0]);
-    exit(EXIT_FAILURE);
-  }
-
-  const char *input_file = argv[1];
-  const char *output_file = argv[argc - 1];
-  const char *command = argv[2];
-
-  int fds[2];
-  if (pipe(fds) == -1) {
-    perror("pipe failed");
-    exit(EXIT_FAILURE);
-  }
-
-  pid_t pid = fork();
-  if (pid < 0) {
-    perror("fork failed");
-    exit(EXIT_FAILURE);
-  }
-
-  if (pid == 0) {
-    close(fds[1]);
-    dup2(fds[0], STDIN_FILENO);
-    close(fds[0]);
-
-    redirect(NULL, output_file);
-
-    char *cmd[1000];
-    split(command, cmd, ' ');
-
-    char abs_path[PATH_MAX];
-    if (find_absolute_path(cmd[0], abs_path)) {
-      free(cmd[0]);
-      cmd[0] = strdup(abs_path);
-    } else {
-      fprintf(stderr, "Command not found: %s\n", cmd[0]);
-      exit(EXIT_FAILURE);
-    }
-
-    if (execv(cmd[0], cmd) < 0) {
-      perror("execv failed");
-      for (int i = 0; cmd[i] != NULL; i++) free(cmd[i]);
-      exit(EXIT_FAILURE);
-    }
-  } else {
-    close(fds[0]);
-
-    int fdin;
-    if (strcmp(input_file, "-") == 0) {
-      fdin = STDIN_FILENO;
-    } else {
-      fdin = open(input_file, O_RDONLY);
-      if (fdin < 0) {
-        perror("Failed to open input file");
+    if (argc < 4) {
+        fprintf(stderr, "Usage: %s <input_file> <command> <output_file>\n",
+                argv[0]);
         exit(EXIT_FAILURE);
-      }
     }
 
-    char buffer[256];
-    ssize_t bytes_read;
-    while ((bytes_read = read(fdin, buffer, sizeof(buffer))) > 0) {
-      write(fds[1], buffer, bytes_read);
+    const char *input_file  = argv[1];
+    const char *output_file = argv[argc - 1];
+    const char *command     = argv[2];
+
+    int fds[2];
+    if (pipe(fds) == -1) {
+        perror("pipe failed");
+        exit(EXIT_FAILURE);
     }
 
-    if (fdin != STDIN_FILENO) {
-      close(fdin);
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork failed");
+        exit(EXIT_FAILURE);
     }
-    close(fds[1]);
-    int status;
-    waitpid(pid, &status, 0);
-    return WIFEXITED(status) ? WEXITSTATUS(status) : -1;
-  }
+
+    if (pid == 0) {
+        close(fds[1]);
+        dup2(fds[0], STDIN_FILENO);
+        close(fds[0]);
+
+        redirect(NULL, output_file);
+
+        char *cmd[1000];
+        split(command, cmd, ' ');
+
+        char abs_path[PATH_MAX];
+        if (find_absolute_path(cmd[0], abs_path)) {
+            free(cmd[0]);
+            cmd[0] = strdup(abs_path);
+        } else {
+            fprintf(stderr, "Command not found: %s\n", cmd[0]);
+            exit(EXIT_FAILURE);
+        }
+
+        if (execv(cmd[0], cmd) < 0) {
+            perror("execv failed");
+            for (int i = 0; cmd[i] != NULL; i++)
+                free(cmd[i]);
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        close(fds[0]);
+
+        int fdin;
+        if (strcmp(input_file, "-") == 0) {
+            fdin = STDIN_FILENO;
+        } else {
+            fdin = open(input_file, O_RDONLY);
+            if (fdin < 0) {
+                perror("Failed to open input file");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        char buffer[256];
+        ssize_t bytes_read;
+        while ((bytes_read = read(fdin, buffer, sizeof(buffer))) > 0)
+            write(fds[1], buffer, bytes_read);
+
+        if (fdin != STDIN_FILENO)
+            close(fdin);
+        close(fds[1]);
+        int status;
+        waitpid(pid, &status, 0);
+        return WIFEXITED(status) ? WEXITSTATUS(status) : -1;
+    }
 }
--- redir.c
+++ redir.c
@@ -11,80 +11,80 @@
 #endif
 
 void redirect(const char *input_file, const char *output_file) {
-  if (input_file && strcmp(input_file, "-") != 0) {
-    int fdin = open(input_file, O_RDONLY);
-    if (fdin < 0) {
-      perror("Failed to open input file");
-      exit(EXIT_FAILURE);
+    if (input_file && strcmp(input_file, "-") != 0) {
+        int fdin = open(input_file, O_RDONLY);
+        if (fdin < 0) {
+            perror("Failed to open input file");
+            exit(EXIT_FAILURE);
+        }
+        dup2(fdin, STDIN_FILENO);
+        close(fdin);
     }
-    dup2(fdin, STDIN_FILENO);
-    close(fdin);
-  }
 
-  if (output_file && strcmp(output_file, "-") != 0) {
-    int fdout = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (fdout < 0) {
-      perror("Failed to open output file");
-      exit(EXIT_FAILURE);
+    if (output_file && strcmp(output_file, "-") != 0) {
+        int fdout = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (fdout < 0) {
+            perror("Failed to open output file");
+            exit(EXIT_FAILURE);
+        }
+        dup2(fdout, STDOUT_FILENO);
+        close(fdout);
     }
-    dup2(fdout, STDOUT_FILENO);
-    close(fdout);
-  }
 }
 
 void add_character_to_string(char *str, char c, size_t max_size) {
-  int len = strlen(str);
-  str[len] = c;
-  str[len + 1] = '\0';
+    int len      = strlen(str);
+    str[len]     = c;
+    str[len + 1] = '\0';
 }
 
 void split(const char *cmd, char *words[], char delimiter) {
-  int word_count = 0;
-  char current_word[10000] = "";
-  const char *next_char = cmd;
+    int word_count           = 0;
+    char current_word[10000] = "";
+    const char *next_char    = cmd;
 
-  while (*next_char != '\0') {
-    if (*next_char == delimiter) {
-      if (strlen(current_word) > 0) {
-        words[word_count++] = strdup(current_word);
-        current_word[0] = '\0';
-      }
-    } else {
-      add_character_to_string(current_word, *next_char, sizeof(current_word));
+    while (*next_char != '\0') {
+        if (*next_char == delimiter) {
+            if (strlen(current_word) > 0) {
+                words[word_count++] = strdup(current_word);
+                current_word[0]     = '\0';
+            }
+        } else {
+            add_character_to_string(current_word, *next_char,
+                                    sizeof(current_word));
+        }
+        ++next_char;
     }
-    ++next_char;
-  }
 
-  if (strlen(current_word) > 0) {
-    words[word_count++] = strdup(current_word);
-  }
-  words[word_count] = NULL;
+    if (strlen(current_word) > 0)
+        words[word_count++] = strdup(current_word);
+    words[word_count] = NULL;
 }
 
 int find_absolute_path(const char *cmd, char *absolute_path) {
-  char *path = getenv("PATH");
-  if (!path) {
-    fprintf(stderr, "No PATH environment variable found.\n");
+    char *path = getenv("PATH");
+    if (!path) {
+        fprintf(stderr, "No PATH environment variable found.\n");
+        strcpy(absolute_path, cmd);
+        return 0;
+    }
+
+    size_t len = strlen(path);
+    char temp_path[PATH_MAX];
+    size_t start = 0;
+
+    for (size_t ix = 0; ix <= len; ++ix) {
+        if (path[ix] == ':' || path[ix] == '\0') {
+            snprintf(temp_path, sizeof(temp_path), "%.*s/%s", (int)(ix - start),
+                     path + start, cmd);
+            if (access(temp_path, X_OK) == 0) {
+                strcpy(absolute_path, temp_path);
+                return 1;
+            }
+            start = ix + 1;
+        }
+    }
+
     strcpy(absolute_path, cmd);
     return 0;
-  }
-
-  size_t len = strlen(path);
-  char temp_path[PATH_MAX];
-  size_t start = 0;
-
-  for (size_t ix = 0; ix <= len; ++ix) {
-    if (path[ix] == ':' || path[ix] == '\0') {
-      snprintf(temp_path, sizeof(temp_path), "%.*s/%s", (int)(ix - start),
-               path + start, cmd);
-      if (access(temp_path, X_OK) == 0) {
-        strcpy(absolute_path, temp_path);
-        return 1;
-      }
-      start = ix + 1;
-    }
-  }
-
-  strcpy(absolute_path, cmd);
-  return 0;
 }
